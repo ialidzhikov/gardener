6b4162704c26deec11f4801b84ca5f47f37102da
Viktor <v.kostov@sap.com>
2026-04-02 10:42:43 +0300

    feature: gardenadm discover exports relevant backupentry and backupbucket

--- pkg/gardenadm/cmd/discover/discover_test.go
+++ pkg/gardenadm/cmd/discover/discover_test.go
@@ -289,5 +289,44 @@ var _ = Describe("Discover", func() {
 				Expect(exists).To(BeTrue(), "for path "+path)
 			}
 		})
+
+		It("should also export backup bucket and backup entry when they exist for the shoot", func() {
+			backupBucket := &gardencorev1beta1.BackupBucket{
+				ObjectMeta: metav1.ObjectMeta{Name: "test-backup-bucket"},
+			}
+			coreBackupEntry := &gardencorev1beta1.BackupEntry{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-backup-entry",
+					Namespace: namespaceName,
+				},
+				Spec: gardencorev1beta1.BackupEntrySpec{
+					BucketName: backupBucket.Name,
+					ShootRef: &corev1.ObjectReference{
+						Name:      shoot.Name,
+						Namespace: shoot.Namespace,
+					},
+				},
+			}
+
+			Expect(fakeClient.Create(ctx, backupBucket)).To(Succeed())
+			Expect(fakeClient.Create(ctx, coreBackupEntry)).To(Succeed())
+
+			Expect(command.Flags().Set("kubeconfig", "some-path-to-kubeconfig")).To(Succeed())
+			Expect(command.RunE(command, []string{shootManifestPath})).To(Succeed())
+
+			Eventually(func() string { return string(stdOut.Contents()) }).Should(SatisfyAll(
+				ContainSubstring("Exported BackupBucket/"+backupBucket.Name),
+				ContainSubstring("Exported BackupEntry/"+coreBackupEntry.Name),
+			))
+
+			for _, path := range []string{
+				fmt.Sprintf("backupbucket-%s.yaml", backupBucket.Name),
+				fmt.Sprintf("backupentry-%s.yaml", coreBackupEntry.Name),
+			} {
+				exists, err := fs.Exists(path)
+				Expect(err).NotTo(HaveOccurred(), "for path "+path)
+				Expect(exists).To(BeTrue(), "for path "+path)
+			}
+		})
 	})
 })
