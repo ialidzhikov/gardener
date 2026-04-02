a63736c78e7a46f5a458c4e21b207a1f76d5df5b
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2026-04-02 16:34:27 +0300

    `gardenadm init`: Add `--bootstrap-only` flag

--- pkg/gardenadm/cmd/init/options.go
+++ pkg/gardenadm/cmd/init/options.go
@@ -29,6 +29,8 @@ type Options struct {
 	// If it has exactly one zone configured, that zone is automatically applied and the flag is optional.
 	// If it has no zones configured, this flag must not be set.
 	Zone string
+
+	BootstrapOnly bool
 }
 
 // ParseArgs parses the arguments to the options.
@@ -87,4 +89,5 @@ func (o *Options) addFlags(fs *pflag.FlagSet) {
 	o.ManifestOptions.AddFlags(fs)
 	fs.BoolVar(&o.UseBootstrapEtcd, "use-bootstrap-etcd", false, "If set, the control plane continues using the bootstrap etcd instead of transitioning to etcd-druid. This is useful for testing purposes to save time.")
 	fs.StringVarP(&o.Zone, "zone", "z", "", "Availability zone for the new node. Required if the control plane worker pool in the `Shoot` has multiple zones configured. Optional if exactly one zone is configured (applied automatically). Must not be set if no zones are configured.")
+	fs.BoolVar(&o.BootstrapOnly, "bootstrap-only", false, "If set, only run the boostrap flow from the init flow.")
 }
