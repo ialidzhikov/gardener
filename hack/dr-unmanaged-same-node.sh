bc1c595d09ec8864f0de343191ec32adde1e6498
Viktor <v.kostov@sap.com>
2026-04-03 11:17:53 +0300

    test: one phase recovery

--- hack/dr-unmanaged-same-node.sh
+++ hack/dr-unmanaged-same-node.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-# Remove old data and secrets
-rm -rf secrets.yaml
-rm -rf data
+# Remove created yaml files
 rm -rf pod.yaml
 
 PF_PID=""
@@ -39,28 +37,6 @@ function targetMachine() {
     sleep 1
     kubectl -n gardenadm-unmanaged-infra exec -it machine-0 -- cat /etc/kubernetes/admin.conf | sed 's/api.root.garden.external.local.gardener.cloud/localhost:6443/' > /tmp/shoot--garden--root.conf
     export KUBECONFIG=/tmp/shoot--garden--root.conf
-}
-
-function copy_data() {
-    # Retry copy until tar doesn't warn "file changed as we read it"
-    max_attempts=20
-    success=0
-    for i in $(seq 1 "$max_attempts"); do
-        rm -rf data
-        output=$(kubectl cp gardenadm-unmanaged-infra/machine-0:/var/lib/etcd-main/data data 2>&1 || true)
-        echo "$output"
-        if echo "$output" | grep -qi 'file changed as we read it'; then
-            echo "Attempt $i/$max_attempts: tar reported 'file changed as we read it', retrying..."
-            sleep 2
-            continue
-        fi
-        success=1
-        break
-    done
-    if [ "$success" -ne 1 ]; then
-        echo "Failed to copy data after $max_attempts attempts due to 'file changed as we read it' error."
-        exit 1
-    fi
 }
 
 # Setup kind and gardenadm
@@ -107,40 +83,12 @@ kubectl -n gardenadm-unmanaged-infra exec -it machine-0  -- gardenadm discover s
 kubectl -n gardenadm-unmanaged-infra exec -it machine-0 -- sh -c 'find . -maxdepth 1 -type f | grep backup | xargs -I {} mv {} gardenadm/resources/'
 kubectl -n gardenadm-unmanaged-infra exec -it machine-0 -- sh -c 'find . -maxdepth 1 -type f | grep shootstate | xargs -I {} mv {} gardenadm/resources/'
 
-# Wait for etcd snapshot to contain the workload data
-echo "Waiting for 6 minutes before copying data to have a backup with more data in it..."
-sleep 360
-
 # Nuke machine
 kubectl -n gardenadm-unmanaged-infra delete pod machine-0 --force
 sleep 3
 
-# First phase of recovery
-kubectl -n gardenadm-unmanaged-infra exec -it machine-0 -- gardenadm init -d /gardenadm/resources --bootstrap
-targetMachine machine-0
-./hack/prep-cluster-2.sh machine-0
+# Recovery (bootstrap + prep + second phase)
+kubectl -n gardenadm-unmanaged-infra exec -it machine-0 -- gardenadm init -d /gardenadm/resources --recover --use-bootstrap-etcd
 
-# Switch to kind
-targetMachine stop
-targetKind
-
-# Transfer data
-copy_data
-
-# Cleanup gardenadm os artefacts
-kubectl -n gardenadm-unmanaged-infra delete pod machine-0 --force
-sleep 3
-
-# Move data to machine-0
-kubectl -n gardenadm-unmanaged-infra exec -it machine-0 -- mkdir -p /var/lib/etcd-main
-kubectl cp data/ gardenadm-unmanaged-infra/machine-0:/var/lib/etcd-main/data
-
-# Second phase of recovery
-kubectl -n gardenadm-unmanaged-infra exec -it machine-0 --  gardenadm init -d /gardenadm/resources  --use-bootstrap-etcd || true
-
-# Remove old data and secrets
-rm -rf secrets.yaml
-rm -rf data
+# Remove created yaml files
 rm -rf pod.yaml
-rm -f dr-unmanaged-backupbucket.yaml
-rm -f dr-unmanaged-backupentry.yaml
