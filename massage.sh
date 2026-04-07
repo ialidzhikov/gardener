fa581b27b23d6072d1fdb0dc0d87c3a119249dc9
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2026-04-07 14:36:46 +0300

    Improve the check if the Pod network is available for a self-hosted Shoot

--- massage.sh
+++ massage.sh
@@ -9,17 +9,17 @@ machine=$1
 # Delete CR that fill interfere with recovery
 kubectl delete csr $(kubectl get csr | grep 'kubernetes.io/kube-apiserver-client ' | cut -f 1 -d ' ' | tr  '\n' ' ')
 # Iterate all ManagedResources across all namespaces
-kubectl get mr -A -o jsonpath='{range .items[*]}{.metadata.namespace}{" "}{.metadata.name}{"\n"}{end}' \
-| while read -r ns name; do
-  echo "Processing MR ${ns}/${name}..."
-  # Remove the gardener-resource-manager finalizer and re-apply
-  kubectl apply -n "$ns" -f <(
-    kubectl get mr -n "$ns" "$name" -o yaml \
-    | sed 's@  - resources.gardener.cloud/gardener-resource-manager.*@@'
-  )
-  # Delete the MR
-  kubectl delete mr -n "$ns" "$name" --ignore-not-found
-done
+# kubectl get mr -A -o jsonpath='{range .items[*]}{.metadata.namespace}{" "}{.metadata.name}{"\n"}{end}' \
+# | while read -r ns name; do
+#   echo "Processing MR ${ns}/${name}..."
+#   # Remove the gardener-resource-manager finalizer and re-apply
+#   kubectl apply -n "$ns" -f <(
+#     kubectl get mr -n "$ns" "$name" -o yaml \
+#     | sed 's@  - resources.gardener.cloud/gardener-resource-manager.*@@'
+#   )
+#   # Delete the MR
+#   kubectl delete mr -n "$ns" "$name" --ignore-not-found
+# done
 kubectl delete node "$machine"
 kubectl get pods --all-namespaces \
   --field-selector=spec.nodeName="$machine" \
