728c3f153973d3b41c1e919cc01c59ef58f58d46
Viktor <v.kostov@sap.com>
2026-04-07 08:17:35 +0300

    cleanup: code afted commit dropping

--- pkg/gardenadm/cmd/init/init.go
+++ pkg/gardenadm/cmd/init/init.go
@@ -81,7 +81,6 @@ func run(ctx context.Context, opts *Options) error {
 func runRecover(ctx context.Context, opts *Options) error {
 	phaseOpts := *opts
 	phaseOpts.Recover = false
-	phaseOpts.Bootstrap = true
 
 	if _, err := bootstrapControlPlane(ctx, &phaseOpts); err != nil {
 		return fmt.Errorf("failed first recovery phase: %w", err)
@@ -103,7 +102,6 @@ func runRecover(ctx context.Context, opts *Options) error {
 		return fmt.Errorf("failed preparing second recovery phase: %w", err)
 	}
 
-	phaseOpts.Bootstrap = false
 	phaseOpts.UseBootstrapEtcd = true
 
 	return runInit(ctx, &phaseOpts)
