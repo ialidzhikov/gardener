f5dff220076b6744a6c7a744e37f303b88642490
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2026-05-21 12:44:09 +0300

    Clean up scheduling hacks for the GRM and the extension Pods (#10)
    * Revert "hack: Schedule extension controllers only on CP nodes"
    
    This reverts commit 23ce5118d5c54e14a0b7b2dd09ef81213601e9d1.
    
    * Revert "bugfix: Verify control plane node exists before deploying GRM"
    
    This reverts commit 88ba75f638eae865aee0e194fc73cf3285795bdd.
    
    * Clean up labeling of the kind cluster's Node with `worker.gardener.cloud/pool=control-plane`

--- pkg/gardenlet/controller/controllerinstallation/controllerinstallation/reconciler.go
+++ pkg/gardenlet/controller/controllerinstallation/controllerinstallation/reconciler.go
@@ -698,16 +698,10 @@ func (r *Reconciler) MutateSpecForSelfHostedShootExtensions(obj runtime.Object) 
 	if r.SelfHostedShootMeta == nil {
 		return nil
 	}
-	if deployment, ok := obj.(*appsv1.Deployment); ok {
-		deployment.Spec.Template.Spec.NodeSelector = map[string]string{
-			"worker.gardener.cloud/pool": "control-plane",
-		}
-	}
 
 	if r.BootstrapControlPlaneNode {
 		if deployment, ok := obj.(*appsv1.Deployment); ok {
 			deployment.Spec.Replicas = ptr.To(int32(1))
-
 			deployment.Spec.Strategy.Type = appsv1.RecreateDeploymentStrategyType
 			deployment.Spec.Strategy.RollingUpdate = nil
 		}
